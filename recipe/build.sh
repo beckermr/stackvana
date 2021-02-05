016a6eebacd1e70ff162eaeb812a483b0e2c897b
Matthew R. Becker <becker.mr@gmail.com>
2021-02-05 17:46:16 -0600

    ENH try this

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 
 export EUPSPKG_NJOBS=2
-stackvana-build lsst_distrib
 
-ln -s ${PREFIX}/include/eigen3/Eigen ${PREFIX}/include/Eigen 
+# ln -s ${PREFIX}/include/eigen3/Eigen ${PREFIX}/include/Eigen
+# ls -lah ${PREFIX}/include
+# exit 1
+
+###############################################################################
+# now build eigen and symlink it to where it can be found by default
+echo "
+Building eigen and making the symlinks..."
+eups distrib install -v -t ${LSST_DM_TAG} eigen
+if [[ `uname -s` == "Darwin" ]]; then
+    eigendir="${EUPS_PATH}/DarwinX86/eigen/3.3.7.lsst2"
+else
+    eigendir="${EUPS_PATH}/Linux64/eigen/3.3.7.lsst2"
+fi
+ln -s ${eigendir}/include/Eigen ${PREFIX}/include/Eigen
+
+stackvana-build lsst_distrib
 
 # get the (de)activate scripts
 for CHANGE in "activate" "deactivate"; do
