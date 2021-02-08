cfb441759479df62a37a26c9aa1746c068e01046
Matthew R. Becker <beckermr@users.noreply.github.com>
2021-02-08 11:13:23 -0600

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -18,8 +18,7 @@ else
 fi
 ln -s ${eigendir}/include/eigen3/Eigen ${PREFIX}/include/Eigen
 
-eups distrib install -v -t ${LSST_DM_TAG} kht
-stackvana-build pipe_tasks
+stackvana-build kht
 
 # get the (de)activate scripts
 for CHANGE in "activate" "deactivate"; do
